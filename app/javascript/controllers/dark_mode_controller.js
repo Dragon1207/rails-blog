2db86cd8536c1bdcdc1da072634c4c1ef143f0d4
Dragon1207 <smileboth0805@outlook.com>
2022-06-24 14:47:48 -0500

    Adding dark and light mode

---
+++ app/javascript/controllers/dark_mode_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "html", "icon" ]
+
+  initialize() {
+    this.darkMode = localStorage.getItem("darkMode") === "true" || false;
+    if (this.darkMode) {
+      this.enableDarkMode();
+    }
+  }
+
+  disableDarkMode() {
+    this.htmlTarget.classList.remove("dark");
+    this.htmlTarget.style.setProperty("color-scheme", "light");
+    this.iconTarget.classList.replace("fa-sun-bright", "fa-moon");
+  }
+
+  enableDarkMode() {
+    this.htmlTarget.classList.add("dark");
+    this.htmlTarget.style.setProperty("color-scheme", "dark");
+    this.iconTarget.classList.replace("fa-moon", "fa-sun-bright");
+  }
+
+  toggle() {
+    if (this.darkMode) {
+      this.darkMode = false;
+      this.disableDarkMode();
+      localStorage.setItem("darkMode", false);
+    } else {
+      this.darkMode = true;
+      this.enableDarkMode();
+      localStorage.setItem("darkMode", true);
+    }
+  }
+}
