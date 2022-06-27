62ada57d8dfdb81e9df016b95513f4002f9a2e29
Dragon1207 <smileboth0805@outlook.com>
2022-06-27 09:27:52 -0500

    Add active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   resources :posts, only: [:index, :show], param: :slug
   resources :tags, only: [:index, :show], param: :slug
   resources :projects, only: [:index, :show], param: :slug
 
   get '/about', to: 'pages#about'
+
+  # Admin routing
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 
   # Defines the root path route ("/")
   root 'pages#home'
