78fbf02a6023e6a1d66f28bc2583bb26b0196f94
Dragon1207 <smileboth0805@outlook.com>
2022-06-27 09:15:24 -0500

    Remove users

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :timeoutable, and :omniauthable
-  devise :database_authenticatable, :lockable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-end
